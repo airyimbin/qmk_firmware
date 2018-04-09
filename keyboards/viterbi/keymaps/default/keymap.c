128da82bc4064a11435c3f5b92af564e5f7a206d
Tim <airyimbin@gmail.com>
2018-04-09 16:52:50 -0400

    spacing

--- keyboards/viterbi/keymaps/default/keymap.c
+++ keyboards/viterbi/keymaps/default/keymap.c
@@ -159,11 +159,11 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
       return false;
       break;
     case RAISE:
-	if (record->event.pressed) {
-	    persistent_default_layer_set(1UL<<_RAISE);
-	}
-	return false;
-	break;
+      if (record->event.pressed) {
+          persistent_default_layer_set(1UL<<_RAISE);
+      }
+      return false;
+      break;
     case ADJUST:
       if (record->event.pressed) {
         layer_on(_ADJUST);
